97d7c32a9d417ea79c834348571edccaca03caca
Levi Gross <levi@levigross.com>
2019-12-15 12:30:09 -0500

    Ready for a release

--- dict_test.go
+++ dict_test.go
@@ -1,14 +1,10 @@
 package dict
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestFirstByte(t *testing.T) {
-	if DictionaryBytes[0] != 115 {
-		t.Fatal("Byte isn't expected")
-	}
 	if len(DictionaryBytes) != 3193759 {
 		t.Fatal("Len of the bytes should be 3193759")
 	}
